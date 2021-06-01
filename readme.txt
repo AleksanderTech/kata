dbfbaa3f7cfda740da26a8e1f2d50c8fe23d9262
AleksanderTech <aleksanderroztrop@gmail.com>
2021-06-01 23:59:48 +0200

    Init

---
+++ readme.txt
@@ -0,0 +1,11 @@
+- Couple of malicious imps did mess in our project and your job is restore it to its former glory
+- Code compilation was tested in node 10 and 12
+- Install dependencies using "npm ci"
+- Compile assets using "npm run build"
+- For your convenience use "npm run watch" while coding
+- Files to refactor are located in /src directory
+- To preview result run index.html in your browser
+- After refactor index.html should look exactly the same
+- Structure of matches data should stay intact (that doesn't mean you can't move it around tho!)
+
+Good luck!
---
+++ src/parser.js
@@ -0,0 +1,84 @@
+// ===SOLID===
+export const fourQuartersFormatter = {
+    formatScore(score) {
+        return score[0][0] + ',' + score[0][1] + ',' + score[1][0] + ',' + score[1][1];
+    }
+}
+
+export const fourSetsFormatter = {
+    formatScore(score) {
+        const scores = /([0-9]+:[0-9]+),([0-9]+:[0-9]+),([0-9]+:[0-9]+),([0-9]+:[0-9]+)/.exec(score);
+        const set1 = scores[2];
+        const set2 = scores[3];
+        const set3 = scores[4];
+
+        return "Main score: " + scores[1] + " ("
+            + "set1 " + set1 + ", "
+            + "set2 " + set2 + ", "
+            + "set3 " + set3
+            + ")";
+    }
+}
+
+export const simpleFormatter = {
+    formatScore(score) {
+        return score;
+    }
+}
+
+export const dashEventNameMaker = {
+    makeEventName(participant1, participant2) {
+        return participant1 + " - " + participant2;
+    }
+}
+
+export const vsEventNameMaker = {
+    makeEventName(participant1, participant2) {
+        return participant1 + " vs " + participant2;
+    }
+}
+
+export class SportScore {
+    constructor(sport, participant1, participant2, score, formatter, eventNameMaker) {
+        this.sport = sport;
+        this.participant1 = participant1;
+        this.participant2 = participant2;
+        this.score = score;
+        this.formatter = formatter;
+        this.eventNameMaker = eventNameMaker;
+    }
+}
+
+export const matches = [
+    new SportScore('soccer', 'Chelsea', 'Arsenal', '2:1', simpleFormatter, dashEventNameMaker),
+    new SportScore('volleyball', 'Germany', 'France', '3:0,25:23,25:19,25:21', fourSetsFormatter, dashEventNameMaker),
+    new SportScore('handball', 'Pogoń Szczeciń', 'Azoty Puławy', '34:26', simpleFormatter, vsEventNameMaker),
+    new SportScore('basketball', 'GKS Tychy', 'GKS Katowice', [['9:7', '2:1'], ['5:3', '9:9']], fourQuartersFormatter, dashEventNameMaker),
+    new SportScore('tennis', 'Maria Sharapova', 'Serena Williams', '2:1,7:6,6:3,6:7', fourSetsFormatter, vsEventNameMaker),
+    new SportScore('ski jumping')
+];
+
+// Closed for changes, open for extensibility
+export default class EventParser {
+
+    makeEventName(match) {
+        try {
+            return match.eventNameMaker.makeEventName(match.participant1, match.participant2);
+        } catch (err) {
+            // error handling
+            return "Exception: invalid sport";
+        }
+
+    }
+
+    formatScore(match) {
+        try {
+            return match.formatter.formatScore(match.score);
+        } catch (err) {
+            // error handling
+            return "Exception: invalid sport";
+        }
+
+    }
+}
+// ===SOLID===
